bdeb83a64847a3315654fecfc6e91e28f5546d54
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-05-21 08:15:51 -0500

    Códigos en clase

---
+++ Progra 21.05.22/Code in class.py
@@ -0,0 +1,32 @@
+DATA = [
+    ["Alumno", "PC1", "PC2", "Asistencia", "Distrito"],
+    ["David", 15,16, True, "San Borja"],
+    ["Randy", 20,18, True, "Callao"],
+    ["Dante", 18,19, False, "Canta"],
+    ["Andre", 18,10, True, "Canta"]
+]
+
+# print(DATA[3][3])
+#
+# for i in range(5):
+#     print(DATA[i][0])
+#
+# for i in range(5):
+#     print(DATA[1][i], end=" ")
+
+for i in range(5):
+    for j in range(5):
+        print(DATA[i][j], end="\t\t")
+    print()
+
+print("##########################")
+
+for i in range(5):
+    print(DATA[i][0],"\t",DATA[i][1],"\t",DATA[i][2],"\t",DATA[i][3],"\t",DATA[i][4])
+
+print("##########################")
+
+for i in range(5):
+    for j in range(5):
+        print('{:>7}'.format(DATA[i][j]), end="\t\t")
+    print()
